241cea33f50b6e255aaa7a48f579db0de6a80c0e
Nathan Harwit <nharwit@gmail.com>
2023-02-24 20:37:07 -0700

    3rd commit, 8 minutes to run, double speed of original code

--- combined_code_allInOnePlace.py
+++ combined_code_allInOnePlace.py
@@ -1,17 +1,17 @@
-# 1. If selenium webdriver only has to load once, it should be significantly faster than doing it multiple times
+# 1. If selenium webdriver only has to load once, it should be significantly faster than it loading multiple times
 # 2. Needed code / info:
 # booth_numbers, company_name, exhibit_urls, description_text, company_website_url, company_contact
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 exhibits_per_page = 1500
 pages = 1
 
 main_page_url = f'https://spie.org/conferences-and-exhibitions/photonics-west/exhibitions/photonics-west-exhibition' \
                 f'/exhibitors?term=&pageSize={exhibits_per_page}&pagesVisited={pages}&sortBy=Relevance'
-
 
 browser = webdriver.Chrome()
 browser.get(main_page_url)
@@ -20,18 +20,19 @@ result = browser.page_source
 
 doc = BeautifulSoup(result, "html.parser")
 
+# exhibit links are derived by clicking on the company name (they have the name of the company as the url text!)
 exhibit_links = doc.find_all('a', {'class': 'subtitle2 companyNameText link linkBlackToBlueNoUnderline'})
-# redundant line below?
-company_names = doc.find_all('a', class_='subtitle2 companyNameText link linkBlackToBlueNoUnderline')
+# redundant line below since they use the same part of the html
+# company_names = doc.find_all('a', class_='subtitle2 companyNameText link linkBlackToBlueNoUnderline')
 company_descriptions = doc.find_all('div', class_='col-12 col-lg-7')
 
-
 exhibit_urls = []  # done
-booth_numbers = []  # done
 company_name = []  # done
 description_text = []  # done
-company_website_url = []
-company_contact = []
+
+booth_numbers = []  # done
+company_website_url = []  # done
+company_contact = []  # done
 
 # Link to company page that includes: booth numbers, contact info, company website link
 base_url = "https://spie.org"
@@ -39,8 +40,8 @@ for link in exhibit_links:
     exhibit_url = base_url + link['href']
     exhibit_urls.append(exhibit_url)
 
-# Company Name
-for name in company_names:
+# Company Name  (modified the below from for name in company_names  --> to "for name in exhibit_links:" )
+for name in exhibit_links:
     names = name.text
     company_name.append(names)
 
@@ -56,29 +57,27 @@ for description in company_descriptions:
 
 # Booth Number
 for url in exhibit_urls:
-#for i in range(1, 100):  # these 3 lines are to test a small subsection of the output so it doesn't take 6-7 minutes to run all the booth numbers once
-    # booth_url = exhibit_urls[i]
-    # result = requests.get(booth_url)
+
     result = requests.get(url)
     content = result.content
     doc = BeautifulSoup(content, 'html.parser')
 
-    div_element = doc.find('div', {'class': 'col-12 col-lg-8 mb30'})    # finding these without using Selenium,
-                                                                        # has diff class compared to if using Selenium?
+    div_element_booth = doc.find('div', {'class': 'col-12 col-lg-8 mb30'})
 
-    if div_element == None:
+    #  Company contact info (website link and location)
+    div_element_contact_info = doc.find('div', {'class': 'col-12 col-lg-4 mb80'})
+
+    if div_element_booth == None:
         booth_number = "No Booth Number Found"
         # print(booth_number)
         booth_numbers.append(booth_number)
     else:
-        span_element = div_element.find(
-            'span')  # find the span element that's within the div element of specified class above
-        if span_element == None:
+        span_element_booth = div_element_booth.find('span')  # find the span element that's within the div element
+        if span_element_booth == None:
             booth_number = "No Booth Number Found"
-            # print(booth_number)
             booth_numbers.append(booth_number)
         else:
-            booth_text = span_element.text  # Find the text of the span element, which is the booth number text below
+            booth_text = span_element_booth.text  # Find the text of the span element, which is the booth number text below
             booth_text = booth_text.strip().replace('\r\n', '')  # replaces thes "\r\n" at the end of the booth number output
             # print(booth_text) # bug testing line to see total output
             # booth_number = booth_text.split(":")[1].strip().split(" ")[0]] original code, doesn't work if more than one booth number
@@ -87,8 +86,50 @@ for url in exhibit_urls:
             # print(booth_number)
             booth_numbers.append(booth_number)
 
-    # counter += 1   # for bug testing -> print out counter with booth number output to figure out which ones are bugged
-# print(booth_numbers)
-# print(f"Counter is: {counter}")
+    # Finds website link below:
+    span_element_contact_info = div_element_contact_info.find('span')  # span element within this SPECIFIC div element to find website
+    if span_element_contact_info == None:  # if span_element doesn't exist then there's no website link
+        href_link = "No website link found"
+        company_website_url.append(href_link)
+    else:
+        a_element = span_element_contact_info.find('a')  # a tag within the span element, will throw error if no website link
+        href_link = a_element.get('href')
+        company_website_url.append(href_link)
+
+    # Finds contact / location information (excluding the link to company website)
+    address_tag = doc.find('address')
+
+    # if the span tag exists, then remove it
+    if span_element_contact_info != None:
+        span_element_contact_info.decompose()
+
+    # some text formatting I grabbed from chatGPT, not sure why it works
+    text_nodes = []
+    for child in address_tag.children:
+        text_nodes.append(child.get_text(strip=True))
+
+    # join with an extra space between each "line" of text
+    address_text = ' '.join(text_nodes)
+    company_contact.append(address_text)
+
+# Output to CSV (panda dataframe below)
+
+data = {
+    # Add index column here, or excel just works?
+    'Booth Number(s)': booth_numbers,
+    'Company Name': company_name,
+    'Link to Company': ['=HYPERLINK("{}")'.format(url) for url in exhibit_urls],
+    'Description': description_text,
+    'Notes': None,
+    'Website link': ['=HYPERLINK("{}")'.format(url) for url in company_website_url],
+    'Company Contact Information': company_contact,
+
+}
+
+# create the dataframe
+df = pd.DataFrame(data)
+
+# convert dataframe to csv
+df.to_csv(r'C:\Users\nharw\Desktop\SPIE_West_Revised_Faster1.csv', index=False)
 
 
